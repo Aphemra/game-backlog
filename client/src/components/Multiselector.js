dbe381f843ee63cbb799ad3a567c85b5c0250bd2
aphemra <aphemra@gmail.com>
2022-11-02 15:38:21 -0400

    Added placeholder routes to server

--- client/src/components/Multiselector.js
+++ client/src/components/Multiselector.js
@@ -1,3 +1,5 @@
 export default function Multiselector() {
+	// Row of platform icons that can be selected or not
+	// Selected icons are added or removed from an array state.
 	return <div>Multiselector</div>;
 }
